0d815da62ed212d014b06fb877afe5cc09ebf48f
Aidan Fleming <ajfleming13@gmail.com>
2020-12-03 11:35:37 -0500

    Random Generator Class in Helper 2.0

--- ADSBackend/Helpers/RandomString.cs
+++ ADSBackend/Helpers/RandomString.cs
@@ -5,7 +5,14 @@ using System.Threading.Tasks;
 
 namespace ADSBackend.Helpers
 {
-    public class RandomString
-    {
-    }
+	public static class RandomString
+	{
+		private static Random random = new Random();
+		public static string Generate(int length)
+		{
+			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+			return new string(Enumerable.Repeat(chars, length)
+				  .Select(s => s[random.Next(s.Length)]).ToArray());
+		}
+	}
 }
