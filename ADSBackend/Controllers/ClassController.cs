8b5180f69a56f1b5504848f11eafcbdf11a9ff32
Joshua Averion <joshaver24@gmail.com>
2020-12-21 17:06:14 -0500

    Added Classes view

---
+++ ADSBackend/Controllers/ClassController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ADSBackend.Data;
+using ADSBackend.Models;
+
+namespace ADSBackend.Controllers
+{
+    public class ClassController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClassController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Class
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Class.ToListAsync());
+        }
+
+        // GET: Class/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class
+                .FirstOrDefaultAsync(m => m.teacherName == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // GET: Class/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Class/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("teacherName,Block,className,roomNumber")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@class);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@class);
+        }
+
+        // GET: Class/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class.FindAsync(id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+            return View(@class);
+        }
+
+        // POST: Class/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("teacherName,Block,className,roomNumber")] Class @class)
+        {
+            if (id != @class.teacherName)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@class);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClassExists(@class.teacherName))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@class);
+        }
+
+        // GET: Class/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class
+                .FirstOrDefaultAsync(m => m.teacherName == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // POST: Class/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var @class = await _context.Class.FindAsync(id);
+            _context.Class.Remove(@class);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassExists(string id)
+        {
+            return _context.Class.Any(e => e.teacherName == id);
+        }
+    }
+}
