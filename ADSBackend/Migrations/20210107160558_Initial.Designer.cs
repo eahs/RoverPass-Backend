9e3ed9a615bf34d57df87f59e9d1bce70c338fec
Michael Tanczos <mike@logic-gate.com>
2021-01-07 11:06:33 -0500

    Collapsed migrations

--- ADSBackend/Migrations/20210107160558_Initial.Designer.cs
+++ ADSBackend/Migrations/20210107160558_Initial.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ADSBackend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20210105185600_AddedPeriod")]
-    partial class AddedPeriod
+    [Migration("20210107160558_Initial")]
+    partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
