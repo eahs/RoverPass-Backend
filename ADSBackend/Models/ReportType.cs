e2eaed2a36e69d5b0b3bbbe40ed8c24603ce2c28
Taranveer <robinflora0529@gmail.com>
2021-02-19 10:51:02 -0500

    Added Report Types

---
+++ ADSBackend/Models/ReportType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models
+{
+    public class ReportType
+    {
+       
+            [Key]
+            public int ReportTypeId { get; set; }
+            public String Name { get; set; }
+            public bool IsEnabled { get; set; }
+        
+    }
+}
